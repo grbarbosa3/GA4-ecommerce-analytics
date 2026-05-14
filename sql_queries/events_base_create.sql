3363eb089e70442b71c93cc9480253f14f03a505
Guilherme Rogério Barbosa <62966035+grbarbosa3@users.noreply.github.com>
2026-05-14 20:35:03 -0300

    Update events_base_create.sql

--- sql_queries/events_base_create.sql
+++ sql_queries/events_base_create.sql
@@ -1,1 +1,31 @@
+CREATE OR REPLACE TABLE `ga4journey.raw.events_base` AS
+SELECT
+  PARSE_DATE('%Y%m%d', event_date) AS event_date,
+  TIMESTAMP_MICROS(event_timestamp) AS event_timestamp,
+  event_name,
+  user_pseudo_id,
 
+  (
+    SELECT value.int_value
+    FROM UNNEST(event_params)
+    WHERE key = 'ga_session_id'
+  ) AS ga_session_id,
+
+  (
+    SELECT value.string_value
+    FROM UNNEST(event_params)
+    WHERE key = 'page_title'
+  ) AS page_title,
+
+  (
+    SELECT value.string_value
+    FROM UNNEST(event_params)
+    WHERE key = 'page_location'
+  ) AS page_location,
+
+  device.category AS device_category,
+  geo.country AS country,
+  ecommerce.transaction_id,
+  ecommerce.purchase_revenue
+FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE _TABLE_SUFFIX BETWEEN '20201101' AND '20210131';
